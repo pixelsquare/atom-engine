14a1308ba37c22ced2035a6a6352f57e22a072b5
pixelsquare <anthony_0205@yahoo.com>
2018-06-25 08:40:04 +0800

    Added linux definitions

---
+++ src/core/PlatformDefine-linux.h
@@ -0,0 +1,17 @@
+/*
+* Copyright (C) 2012 PixelSquareLabs - All Rights Reserved
+*
+* Created: 28/06/2018
+* Author:  Anthony Ganzon
+* Email:   pixelsquarelabs@yahoo.com
+*          pixelsquarelabs@gmail.com
+*/
+
+#ifndef _ATOM_PLATFORM_DEFINE_LINUX_H_
+#define _ATOM_PLATFORM_DEFINE_LINUX_H_
+
+#if defined(ATOM_STATIC)
+#define ATOMAPI
+#endif
+
+#endif // _ATOM_PLATFORM_DEFINE_LINUX_H_
